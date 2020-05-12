6aa7371c38351811e119c136433bff7328a5b78d
oasis <799643542@qq.com>
2020-05-12 22:50:49 +0800

    photo url + debug

--- api/SqlUtils.py
+++ api/SqlUtils.py
@@ -47,7 +47,7 @@ def search(key,table):#key is a dict variable used to search required row in tar
             for customer in customers:
                 customer_dict = customer.__dict__
                 if customer_dict["customer_image_path"] is not None:
-                    customer_dict["customer_image_path"] = customer_dict["id"]
+                    customer_dict["customer_image_path"] = "http://127.0.0.1/api/customer/image/"+   str(customer_dict["id"])
                 r.append(customer_dict)
             return r
         elif table == "employee":
@@ -71,8 +71,8 @@ def search(key,table):#key is a dict variable used to search required row in tar
 
                 temp["appointment_date"] = temp["appointment_date"].timestamp()
                 temp["date"] = temp["date"].timestamp()
-                if temp.get("pet_image_path") is not None:
-                    temp["pet_image_path"]=temp["app_primary_key"]
+                if temp.get("pet_image_path") is  None:
+                    temp["pet_image_path"]= "http://127.0.0.1/api/appointment/image/" + str(temp["app_primary_key"])
 
                 r.append(temp)
             # if index is None:
@@ -135,13 +135,12 @@ def search(key,table):#key is a dict variable used to search required row in tar
             if key.get("release_time") is not None:
                 key["release_time"] = datetime.datetime.fromtimestamp(key["release_time"])
             operations = Operation.query.filter_by(**key).all()
-
             for operation in operations:
                 operation_dict = operation.__dict__
                 operation_dict["surgery_begin_time"] = operation_dict["surgery_begin_time"].timestamp()
                 operation_dict["release_time"] = operation_dict["release_time"].timestamp()
                 r.append(operation_dict)
-            return r
+            # return r                  
         elif table == "pet":
             r = Pet.query.filter_by(**key).all()
         else:
@@ -517,17 +516,18 @@ def searchTimeSpan(key,table):#The key format should be {"column":"...","start":
 
 def searchImage(key,table):
     table = table.lower()
+    b = None
     if table == "customer":
         customer = Customer.query.filter_by(**key).first()
         if customer.customer_image_path is not None:
             with open(customer.customer_image_path,"rb") as image:
-                b=bytearray(image.read())
+                b=bytes(image.read())
         return b
     elif table=="appointment":
         appointment = Appointment.query.filter_by(**key).first()
         if appointment.pet_image_path is not None:
             with open(appointment.pet_image_path,"rb") as image:
-                b = bytearray(image.read())
+                b = bytes(image.read())
         return b
 
 
--- api/customers.py
+++ api/customers.py
@@ -34,7 +34,7 @@ def user_register():
     if test.lower() != session['image'].lower():
         return status(4103,'Incorrect verification code')
 
-    if DBUtil.search(username,"customer")[0]:
+    if DBUtil.search(username,"customer"):
         return status(4103,'exist username')
     else:
         return_status = DBUtil.insert(register_res,"customer")    
@@ -58,7 +58,7 @@ def user_login():
     if not user:
         return status(4103,'error username or password')
     else:
-        user_id = user[0][0].get('id')                               #咋回事啊   变成两个[0][0] 了
+        user_id = user[0].get('id')                              
         return generate_token(user_id,"customer")
         
 
@@ -81,7 +81,7 @@ def appointment_create():
     pets["pet_species"] = appointment_res.get("species")
 
     succusse = DBUtil.search(pets,"pet")              #没找到才插入
-    print(succusse)
+
     if not succusse:
         DBUtil.insert(pets,"pet")
     
@@ -140,8 +140,11 @@ def user_appointment_get(id):      #id是appointment的id
     # print("-------------------------------------")
     appointments,length = DBUtil.search(inpu,"appointment")
 
+    # print(type(a['pet_i']))
+
     for a in appointments:
         a.pop('_sa_instance_state')
+
     return_appointment = {}
     return_appointment["total"] = length
     return_appointment["count"] = 0 if appointments==0 else len(appointments)
@@ -159,12 +162,10 @@ def user_profile_get():
 
     #获取用户信息
     current_customer = getCustomer()
-
     profile = DBUtil.search({'id':current_customer['id']},'customer')[0]
-    profile[0].pop("_sa_instance_state")
-    profile[0].pop("password_hash")
+    profile.pop("_sa_instance_state")
+    profile.pop("password_hash")
     if profile:
-        print(profile)
         return status(200,'get profile successfully',profile)
     else:
         return status(404)
@@ -272,7 +273,7 @@ def validate_picture():
     im = im.filter(ImageFilter.FIND_EDGES)
     return im, str1
 
-@app.route('/api/customer/code/',methods=['GET'])
+@app.route('/api/customer/code',methods=['GET'])
 def get_code():
     image, str1 = validate_picture()
     # 讲验证码图片以二进制形式写入内存，防止图片都放在文件夹中，占用磁盘空间
@@ -286,6 +287,35 @@ def get_code():
     session['image'] = str1
     return response
 
+@app.route('/api/appointment/image/<int:id>',methods=['GET'])   # appointment 的id
+@auth.login_required
+def pet_image(id):
+
+    # with open('13690.html1.jpg',"rb") as image:
+    #     b=bytes(image.read())
+    # print(type(b))
+    # response = make_response(b)
+    # response.headers['Content-Type'] = 'image/gif'
+    # 测试
+    
+    byte = DBUtil.searchImage({"app_primary_key":id},'appointment')        #传给你  appointment  的 primary key id， 返回二进制
+    if byte is None:
+        return status('403','no photos')
+    response = make_response(byte)
+    response.headers['Content-Type'] = 'image/gif'
+    return response
+
+
+@app.route('/api/customer/image/<int:id>',methods=['GET'])   #  customer自己的
+@auth.login_required
+def customer_image(id):
+
+    byte = DBUtil.searchImage({"id":id},'customer')         #传给你  customer id，  返回二进制
+    if byte is None:
+        return status('403','no photos')
+    response = make_response(byte)
+    response.headers['Content-Type'] = 'image/gif'
+    return response
 
 @app.route('/api/customer/test',methods=['GET'])
 def test():
