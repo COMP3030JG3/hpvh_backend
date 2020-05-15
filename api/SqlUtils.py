47e156e78379b4ff0348ae9d323bd6f27aab99cc
Zou Keming <keming.zou@ucdconnect.ie>
2020-05-15 12:09:13 +0800

    pet image debug, customer image insert (debug)

--- api/SqlUtils.py
+++ api/SqlUtils.py
@@ -47,7 +47,9 @@ def search(key,table):#key is a dict variable used to search required row in tar
             for customer in customers:
                 customer_dict = customer.__dict__
                 if customer_dict["customer_image_path"] is not None:
+                    customer_dict.update({"avatar": customer_dict["customer_image_path"]})
                     customer_dict["customer_image_path"] = "http://47.94.166.75:3000/api/customer/image/"+   str(customer_dict["id"])
+
                 r.append(customer_dict)
             return r
         elif table == "employee":
@@ -56,7 +58,13 @@ def search(key,table):#key is a dict variable used to search required row in tar
                 r = Employee.query.filter_by(**key).first()
                 if not check_password_hash(r.password_hash, password):
                     return 0
-            r = Employee.query.filter_by(**key).all()
+            employees = Employee.query.filter_by(**key).all()
+            for employee in employees:
+                employee_dict = employee.__dict__
+                employee_dict.update({"avatar":""})
+
+                r.append(employee_dict)
+            return r
         elif table == "appointment":
             # preprocessing
             if "appointment_date" in key:
@@ -125,6 +133,7 @@ def search(key,table):#key is a dict variable used to search required row in tar
                 question_dict.update({"user_type":"customer"})
                 question_dict.update({"username":question.questioner.__dict__.get("username")})
                 question_dict.update({"user_id":question_dict.get("questioner_id")})
+                question_dict.update({"replies" : len(question.answers.all())})
 
                 r.append(question_dict)
             return r[(index-1)*15:index*15],len(r)
@@ -160,11 +169,18 @@ def searchAll(table):#select * from table
             for customer in customers:
                 customer_dict = customer.__dict__
                 if customer_dict["customer_image_path"] is not None:
-                    customer_dict["customer_image_path"] = customer_dict["id"]
+                    customer_dict.update({"avatar": customer_dict["customer_image_path"]})
+                    customer_dict["customer_image_path"] = "http://47.94.166.75:3000/api/customer/image/"+   str(customer_dict["id"])
                 r.append(customer_dict)
             return r
         elif table == "employee":
-            r = Employee.query.all()
+            employees = Employee.query.all()
+            for employee in employees:
+                employee_dict = employee.__dict__
+                employee_dict.update({"avatar":""})
+
+                r.append(employee_dict)
+            return r
         elif table == "appointment":
             appointments = Appointment.query.all()
             for appointment in appointments:
@@ -198,6 +214,7 @@ def searchAll(table):#select * from table
                 question_dict.update({"user_type": "customer"})
                 question_dict.update({"username": question.questioner.__dict__.get("username")})
                 question_dict.update({"user_id": question_dict.get("questioner_id")})
+                question_dict.update({"replies": len(question.answers.all())})
                 r.append(question_dict)
             return r
         elif table == "operation":
@@ -367,21 +384,20 @@ def modify(key,data,table):#key is a dict variable used to search required row i
         # data = json.loads(data)
         table = table.lower()
         if table == "customer":
-            print(0)
+            # print(0)
             if key.get("old_password") is not None:
                 old_password = key.pop("old_password")
-                if "password_hash" in data:
-                    new_password = data.pop("password_hash")
-                    if check_password_hash(Customer.query.filter_by(**key).first().password_hash,old_password):
-                        data["password_hash"]= generate_password_hash(str(new_password))
+                new_password = data.pop("password_hash")
+                if check_password_hash(Customer.query.filter_by(**key).first().password_hash,old_password):
+                    data["password_hash"]= generate_password_hash(str(new_password))
             if "customer_image_path" in data:
-                print(1)
+                # print(1)
                 old_image_path = Customer.query.filter_by(**key).first().customer_image_path
                 if old_image_path is not None:
                     os.remove(old_image_path)
                 id = key["id"]
                 with open("uploaded_image\customer_image_path\\" + str(id) + ".jpg", "wb") as image:
-                    print(2)
+                    # print(2)
                     image.write(base64.b64decode(data["customer_image_path"]))
                     data["customer_image_path"] = "uploaded_image\customer_image_path\\" + str(id) + ".jpg"
             Customer.query.filter_by(**key).update(data)
